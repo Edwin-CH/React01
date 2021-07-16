5643d3d0b2d8d808d9ea74ef4004ddeb54f2e5c0
Edwin <edwinyoelchambi@gmail.com>
2021-07-16 08:53:51 -0500

    actualizacion 2 de app js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import C01componente from '../componentes/C01componente';
 import C02contador from '../componentes/C02contador';
-import { Component } from 'react';
 
 function App() {
   return (
