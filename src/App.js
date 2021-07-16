c2a7d6d23847929ce7141ca4dfe022fb0f4955eb
Edwin <edwinyoelchambi@gmail.com>
2021-07-16 08:57:59 -0500

    actualizacion de app y componentes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import C01componente from '../componentes/C01componente';
-import C02contador from '../componentes/C02contador';
+import C01componente from './componentes/C01componente';
+import C02contador from './componentes/C02contador';
 
 function App() {
   return (
