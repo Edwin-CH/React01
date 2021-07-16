097b62499424b2190b01c72580245ff3e1ccf243
Edwin <edwinyoelchambi@gmail.com>
2021-07-16 08:52:42 -0500

    actualizacion de app js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import C01componente from '../componentes/C01componente';
+import C02contador from '../componentes/C02contador';
+import { Component } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Sitio web en react
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <C01componente />
+      <hr/>
+      <C02contador />
     </div>
   );
 }
