639382c19bbf62e8051a04f03982315adbaed08e
Edwin <edwinyoelchambi@gmail.com>
2021-07-16 08:33:28 -0500

    actualizacion app js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import C02contador from '../componentes/C02contador';
 
 function App() {
   return (
-    <div>
-      <C01componente />
-      <hr />
-      <C02contador />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Sitio web en react
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
-
 export default App;
